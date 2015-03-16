c447a3f84a24d89cbcee00ea6571399f00aab83b
Rogue Linux <vance.scotty@gmail.com>
2015-03-16 14:13:41 -0500

    got rid of pixelMatrix.c/h, as the contents could be thrown into imageUtils

--- pixelMatrix.h
+++
@@ -1,9 +0,0 @@
-//pixelMatrix.h gives the XDIM and YDIM constants used for defining the matrices in main.c, and will serve in the future as a place for any raw pixel matrix functions
-
-#ifndef _PIXELMAT_H_
-#define _PIXELMAT_H_
-
-const int XDIM;
-const int YDIM;
-
-#endif
