d5ba65257449d025441ba2a79554c09fe01faa49
Rogue Linux <vance.scotty@gmail.com>
2015-04-10 10:28:42 -0500

    This is about to get UGLY as a just born babies head.  Yeah.  That ugly.  bmp.h is a thing from stackoverflow, link given in nonmain.cpp and bmp.h
    Next commit will be getting rid of imageUtils junk

---
+++ bmp.h
@@ -0,0 +1,28 @@
+//"borrowed" from http://stackoverflow.com/questions/5751749/how-can-i-read-bmp-pixel-values-into-an-array
+
+
+typedef int LONG;
+typedef unsigned short WORD;
+typedef unsigned int DWORD;
+
+typedef struct tagBITMAPFILEHEADER {
+  WORD  bfType;
+  DWORD bfSize;
+  WORD  bfReserved1;
+  WORD  bfReserved2;
+  DWORD bfOffBits;
+} BITMAPFILEHEADER, *PBITMAPFILEHEADER;
+
+typedef struct tagBITMAPINFOHEADER {
+  DWORD biSize;
+  LONG  biWidth;
+  LONG  biHeight;
+  WORD  biPlanes;
+  WORD  biBitCount;
+  DWORD biCompression;
+  DWORD biSizeImage;
+  LONG  biXPelsPerMeter;
+  LONG  biYPelsPerMeter;
+  DWORD biClrUsed;
+  DWORD biClrImportant;
+} BITMAPINFOHEADER, *PBITMAPINFOHEADER;
