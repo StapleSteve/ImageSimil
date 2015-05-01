7b88ff4290af44ba4087c064410874c0182eeadf
StapleSteve <vance.scotty@gmail.com>
2015-05-01 08:16:39 -0500

    Fixed some stuff with the chosen license
    And some other comment stuff

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 /*
  * main.cpp
  * 
- * Copyright 2015 Scotty Vance <svance@rogue>
+ * Part of the Open Source Project "ImageSimil"
+ * 
+ * Copyright 2015 Scotty Vance <vance.scotty@gmail.com>
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  * 
  * This program is distributed in the hope that it will be useful,
